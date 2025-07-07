2dc45e7bde0adc0f1faa6bc598927f85912514d1
KODA\turkyilmaz.ozkan <turkyilmaz.ozkan@inventuna.com>
2025-07-08 00:00:40 +0300

    kamera calisiyor

--- src/screens/BarcodeScannerScreen.js
+++ src/screens/BarcodeScannerScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Alert, StyleSheet, PermissionsAndroid, Platform,Text } from 'react-native';
-import { CameraKitCameraScreen } from 'react-native-camera-kit';
- 
+import { View, Alert, StyleSheet, PermissionsAndroid, Platform, Text } from 'react-native';
+import { Camera, CameraType, CameraKitCameraScreen } from 'react-native-camera-kit';
+
 
 export default function BarcodeScannerScreen({ navigation }) {
   const [scanned, setScanned] = useState(false);
@@ -25,16 +25,19 @@ export default function BarcodeScannerScreen({ navigation }) {
     requestCameraPermission();
   }, []);
 
-  const onBarcodeRead = (event) => {
-    if (scanned) return;
-    setScanned(true);
-    Alert.alert('Barkod', `Kod: ${event.nativeEvent.codeStringValue}`);
-    navigation.goBack();
-  };
+
 
   return (
-    <View style={{ flex: 1 }}>
-          <Text>Home Screen</Text>
+    <View>
+      <Camera
+        scanBarcode={true} 
+        style={{width: '100%', height: '100%'}}
+        onReadCode={(event) => Alert.alert('QR code found')} // optional
+        showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner, that stops when a code has been found. Frame always at center of the screen
+        laserColor='red' // (default red) optional, color of laser in scanner frame
+        frameColor='white' // (default white) optional, color of border of scanner frame
+
+      />
     </View>
   );
 }
